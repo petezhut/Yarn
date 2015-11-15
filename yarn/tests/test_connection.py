94313bbb43ce2238c49d7bb8f97aed509a2cc72e
Jason L McFarland <Jason.McFarland1976+python-yarn@gmail.com>
2015-11-15 12:17:50 -0500

    Still trying

--- yarn/tests/test_connection.py
+++ yarn/tests/test_connection.py
@@ -5,13 +5,13 @@ import getpass
 import unittest
 from yarn.api import env, run, local
 
-env.host_string = local('ifconfig eth0 | grep "inet addr" | cut -d":" -f2 | awk \'{print $1}\'')
-env.host_port = 2222
+env.host_string = local('ifconfig eth0 | grep "inet addr" | cut -d":" -f2 | awk \'{print $1}\'')[0]
 env.password = "123456"
 
 class TestConnection(unittest.TestCase):
 
     def test_simple_connection(self):
+        print(env.__dict__)
         run("uptime")
 
     

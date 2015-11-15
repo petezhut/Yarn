1e96436a60d63cb69c4f748025156c608d1c211e
Jason L McFarland <Jason.McFarland1976+python-yarn@gmail.com>
2015-11-15 12:31:44 -0500

    I feel like I'm getting close

--- yarn/tests/test_connection.py
+++ yarn/tests/test_connection.py
@@ -6,6 +6,7 @@ import unittest
 from yarn.api import env, run, local
 
 env.host_string = local('ifconfig eth0 | grep "inet addr" | cut -d":" -f2 | awk \'{print $1}\'')
+env.host_port = 2222
 env.password = "123456"
 
 class TestConnection(unittest.TestCase):
